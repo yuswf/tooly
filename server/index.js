ea876b562b877eb19e5fd700fe30d0c35408a215
YUSUF <yusu@duck.com>
2022-12-05 18:09:36 +0300

    🧬 tree changed

--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
+const config = require('./config');
+
 const {Client, GatewayIntentBits} = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const axios = require('axios');
+const io = require('socket.io');
 
-const config = {
-    token: 'MTA0NzYwODkzNTUwMDIxODQ5OQ.Go-IND.je6PHS8GkqP2kq2qTHmItrWh3Yxe3nP9t0Emu4'
-}
+
 
 client.on('ready', () => {
-    const items = [];
-    client.guilds.cache.get('1041312550777790524').members.cache.map(member => {
-        items.push(member.user.id);
-    })
-    const id = items[Math.floor(Math.random()*items.length)];
-    client.guilds.cache.get('1041312550777790524').channels.cache.get('1042410829825454090').send(`hayirdir lan samet :haha:`);
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
