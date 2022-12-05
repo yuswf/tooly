d150598dcb0768e8987883e6e564c9fc0c81487f
YUSUF <yusu@duck.com>
2022-12-05 18:21:11 +0300

    server-side

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const config = require('./config');
 
+const app = require('express')();
 const {Client, GatewayIntentBits} = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
 const io = require('socket.io');
 
-
+app.get('/', (req, res) => {
+    res.send('tooly server-side');
+});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(config.token);
+
+app.listen(config.port, () => {
+    console.log(`Listening on port ${config.port}!`);
+});
