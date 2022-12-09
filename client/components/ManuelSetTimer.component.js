a09271b672a6169b4ecf95e09c2670c5c8f3682b
YUSUF <yusu@duck.com>
2022-12-10 01:16:19 +0300

    changed timer limits

--- client/components/ManuelSetTimer.component.js
+++ client/components/ManuelSetTimer.component.js
@@ -48,6 +48,7 @@ function ManuelSetTimerComponent() {
 
         const date = Math.floor(ms / 1000);
         const h = Math.floor(date / 3600) % 24;
+        console.log(h)
         const m = Math.floor(date / 60) % 60;
         const s = Math.floor(date % 60);
 
@@ -100,7 +101,7 @@ function ManuelSetTimerComponent() {
                 <select value={hours}
                         onChange={(e) => setHours(Number(e.target.value))}
                         className="text-sm rounded bg-[#1f2024] w-full p-3.5">
-                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
                         .map((number, index) => (
                             <option key={index} value={number}>{number}</option>
                         ))}
@@ -109,7 +110,7 @@ function ManuelSetTimerComponent() {
                 <select value={minutes}
                         onChange={(e) => setMinutes(Number(e.target.value))}
                         className="text-sm rounded bg-[#1f2024] w-full p-3.5">
-                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
                         .map((number, index) => (
                             <option key={index} value={number}>{number}</option>
                         ))}
@@ -118,7 +119,7 @@ function ManuelSetTimerComponent() {
                 <select value={seconds}
                         onChange={(e) => setSeconds(Number(e.target.value))}
                         className="text-sm rounded bg-[#1f2024] w-full p-3.5">
-                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
                         .map((number, index) => (
                             <option key={index} value={number}>{number}</option>
                         ))}
