7badce52117d1d21746ca42cad873730a0fa8353
YUSUF <yusu@duck.com>
2022-12-13 20:35:25 +0300

    fix tools viewer (admin)

--- client/components/Tools.component.js
+++ client/components/Tools.component.js
@@ -83,7 +83,7 @@ function ToolsComponent() {
         },
     ];
 
-    const [component, setComponent] = useState(!process.env.admins.includes(data.id) ? tools.filter(tool => tool.perm <= 0)[0]?.component || 'No tools available.' : tools.filter(tool => tool.perm === 3)[0].component);
+    const [component, setComponent] = useState(!process.env.admins.includes(data.id) ? tools.filter(tool => tool.perm <= 0)[0]?.component || 'No tools available.' : tools.filter(tool => tool.perm === 3)[0].component || tools[0].component);
     const [i, setI] = useState(!process.env.admins.includes(data.id) ? tools.findIndex(tool => tool.perm <= 0) : tools.findIndex(tool => tool.perm === 3));
 
     const toolViewer = (i) => {
