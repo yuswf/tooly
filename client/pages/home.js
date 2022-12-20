db189abccab2ad0aa7cce0eaadd8763211ba3f66
YUSUF <yusu@duck.com>
2022-12-20 19:20:06 +0300

    fixed home page routing

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,9 +1,16 @@
+import {useEffect} from 'react';
 import {useRouter} from 'next/router';
+
+import LoaderComponent from '../components/Loader.component';
 
 function Home() {
     const router = useRouter();
 
-    return router.push('/');
+    useEffect(() => {
+        return () => router.push('/');
+    }, []);
+
+    return <LoaderComponent />
 }
 
 export default Home;
